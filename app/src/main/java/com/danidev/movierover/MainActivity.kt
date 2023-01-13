ef9892c68b9a043f3360f99a627469207c015aca
Daniyar <dancho.rancho07@gmail.com>
2023-01-13 10:40:53 +0100

    added images with animation

--- app/src/main/java/com/danidev/movierover/MainActivity.kt
+++ app/src/main/java/com/danidev/movierover/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.view.ActionMode
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.GridLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.children
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -52,6 +54,41 @@ class MainActivity : AppCompatActivity() {
                     return@setOnLongClickListener false
                 }
                 false -> return@setOnLongClickListener true
+            }
+        }
+
+        val posterContainer = findViewById<GridLayout>(R.id.center_bar)
+        posterContainer.children.forEach { child ->
+            child.setOnLongClickListener {
+                when (actionMode == null) {
+                    true -> {
+                        actionMode = startActionMode(object: ActionMode.Callback {
+                            override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+                                when (item?.itemId) {
+                                    R.id.ios_share, R.id.file_copy -> Toast.makeText(this@MainActivity, item.title, Toast.LENGTH_SHORT).show()
+                                }
+                                return true
+                            }
+
+                            override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                                val inflater = mode?.menuInflater
+                                inflater?.inflate(R.menu.contextual_toolbar_poster, menu)
+                                mode?.title = "Select Option"
+                                return true
+                            }
+
+                            override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                                return false
+                            }
+
+                            override fun onDestroyActionMode(mode: ActionMode?) {
+                                actionMode = null
+                            }
+                        })
+                        return@setOnLongClickListener false
+                    }
+                    false -> return@setOnLongClickListener true
+                }
             }
         }
     }
